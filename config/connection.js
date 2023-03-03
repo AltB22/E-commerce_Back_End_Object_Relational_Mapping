5400eaa1c18593592ec7a10510704b86c2782fd2
Alt B 2022 <massie.w@gmail.com>
2023-03-03 09:33:50 -0800

    imported sequelize connection and synced models to DB in server.

--- config/connection.js
+++ config/connection.js
@@ -7,6 +7,8 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
+      password: "PASSWORD",//added this not sure if needed here or can only live in .env
+      //Do we need to include DB location here?  or PORT number at least?
       dialectOptions: {
         decimalNumbers: true,
       },
