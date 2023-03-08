1fa6738ded4434c1c76c8ac6730f37d1ddfc1209
Alt B 2022 <massie.w@gmail.com>
2023-03-07 23:42:48 -0800

    Bug fixes in models, models/index.js.  Multiple rounds of testing of routes in insomnia

--- models/Tag.js
+++ models/Tag.js
@@ -2,7 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Tag extends Model {}
+class Tag extends Model { }
 
 Tag.init(
   {
@@ -21,7 +21,7 @@ Tag.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tags',
+    modelName: 'tag',
   }
 );
 
